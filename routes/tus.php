27b4f5c5ff388b6b94e4ddb0330b89994029c830
Boris Blondin <blondin.boris@gmx.com>
2024-05-01 10:31:47 +0100

    fixed middleware in routes

--- routes/tus.php
+++ routes/tus.php
@@ -7,8 +7,7 @@ use KalynaSolutions\Tus\Http\Middleware\ValidateFileSizeMiddleware;
 use KalynaSolutions\Tus\Http\Middleware\ValidateVersionMiddleware;
 
 Route::controller(TusUploadController::class)
-    ->middleware(config('tus.middleware'))
-    ->middleware(ValidateVersionMiddleware::class)
+    ->middleware(array_merge(config('tus.middleware'), [ValidateVersionMiddleware::class]))
     ->prefix(config('tus.path'))
     ->name('tus.')
     ->group(function () {
